d64df762e3d829ffaa432003c9815ce26c7f9b4d
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2023-05-31 07:07:13 +0500

    Add project files.

---
+++ API/Models/DTOs/CreateProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models.DTOs;
+
+public class CreateProduct
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public uint Quantity { get; set; }
+    public decimal Price { get; set; }
+    public IFormFile Photo { get; set; }
+}
---
+++ API/Models/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Models.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public uint Quantity { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+}
